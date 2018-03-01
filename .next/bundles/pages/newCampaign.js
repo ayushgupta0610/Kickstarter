ff45d8fa7ffe18993711d3136d24ef9c3ee2b44d
ayush <ayushgupta0610@gmail.com>
2018-03-01 23:16:08 +0530

    fixing the wrap style of addresses

---
+++ .next/bundles/pages/newCampaign.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/newCampaign', function() {
+            var comp = module.exports=webpackJsonp([6],{844:function(e,t,n){e.exports=n(845)},845:function(e,t,n){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}var a=n(38),r=u(a),o=n(41),l=u(o),s=n(19),c=u(s),f=n(4),i=u(f),d=n(5),p=u(d),h=n(6),m=u(h),v=n(7),_=u(v),g=n(0),y=u(g),k=n(216),w=u(k);!function(e){function t(){return(0,i.default)(this,t),(0,m.default)(this,(t.__proto__||(0,c.default)(t)).apply(this,arguments))}(0,_.default)(t,e),(0,p.default)(t,[{key:"componentDidMount",value:function(){function e(){return t.apply(this,arguments)}var t=(0,l.default)(r.default.mark(function e(){var t;return r.default.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,w.default.methods.getDeployedCampaigns().call();case 2:t=e.sent,console.log(t);case 4:case"end":return e.stop()}},e,this)}));return e}()},{key:"render",value:function(){return y.default.createElement("h1",null," This is the new campaign page! ")}}])}(g.Component)}},[844]);
+            return { page: comp.default }
+          })
+        
